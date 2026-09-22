17b007197bf8ec5e7614dff2ac96792a3b1f5342
Pink00Monster <vanillaWangyi@163.com>
2026-09-22 16:03:28 +0200

    add microsoft website testing code

--- test.rb
+++ test.rb
@@ -1,8 +1,37 @@
 require 'selenium-webdriver'
-driver = Selenium::WebDriver.for :chrome
-driver.get('https://www.google.com')
-puts driver.title
+require 'rspec'
 
-sleep 5
+RSpec.describe 'Microsoft Denmark homepage' do
 
-driver.quit
+  before(:each) do
+    @driver = Selenium::WebDriver.for :chrome
+    @driver.manage.window.maximize
+    @driver.get('https://www.microsoft.com/da-dk')
+  end
+
+  after(:each) do
+    @driver.quit
+  end
+
+  it 'opens the Microsoft Denmark homepage' do
+    expect(@driver.title).to include('Microsoft')
+  end
+
+  it 'opens Microsoft 365 page' do
+    microsoft_365 = @driver.find_element(
+      :css,
+      '[aria-label="Køb Microsoft 365"]'
+    )
+
+    microsoft_365.click
+
+    wait = Selenium::WebDriver::Wait.new(timeout: 10)
+
+    wait.until do
+      @driver.title.include?('Microsoft')
+    end
+
+    expect(@driver.current_url).to include('microsoft.com')
+  end
+
+end
